b89de209243a35a35e49b1f516446418fa1c77f2
- <86375852+sogful@users.noreply.github.com>
2026-06-07 18:07:36 +0300

    semi-support for firefox

--- src/background.js
+++ src/background.js
@@ -1,4 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true}).catch(() => {});
-});
-chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true}).catch(() => {});
+// chrome
+if (typeof chrome !== "undefined" && chrome.sidePanel) {
+  const setbehavior = () => chrome.sidePanel.setPanelBehavior({openPanelOnActionClick: true}).catch(() => {});
+  chrome.runtime.onInstalled.addListener(setbehavior);
+  setbehavior();
+}
+
+// firefox
+else if (typeof browser !== "undefined" && browser.sidebarAction && browser.action) {
+  browser.action.onClicked.addListener(() => browser.sidebarAction.toggle());
+}
